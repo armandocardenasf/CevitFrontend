61e16da1c17ba5097fd7b0b7d16ec37a5e2df6d9
oscark22 <98506040+oscark22@users.noreply.github.com>
2023-04-23 15:27:35 -0700

    light theme default

--- cevit-frontend/src/theme.js
+++ cevit-frontend/src/theme.js
@@ -119,7 +119,7 @@ export const ColorModeContext = createContext({
 });
 
 export const useMode = () => {
-  const [mode, setMode] = useState("dark");
+  const [mode, setMode] = useState("light");
 
   const colorMode = useMemo(
     () => ({
