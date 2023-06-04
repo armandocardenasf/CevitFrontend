6ab65ce0eedf5b340c54f34fa7f60dfc13457d38
Oscar Montoya <98506040+oscark22@users.noreply.github.com>
2023-06-03 21:10:56 -0700

    changed login for jwt. Logout if token is expired

--- cevit-frontend/src/api/url.jsx
+++ cevit-frontend/src/api/url.jsx
@@ -42,9 +42,15 @@ AuthRutaApi.interceptors.request.use(async (config) => {
     oRefreshToken: refreshToken,
   };
 
-  // Validate token to see if it has expired. If it has, get a new token.
+  // If token has expired, logout.
   const decoded = jwt_decode(token);
   if (decoded.exp * 1000 < Date.now()) {
+    handleLogout();
+    return Promise.reject(new Error("Token has expired."));
+  }
+
+  // Token has 10 minutes left to expire, get a new token.
+  if (decoded.exp * 1000 - 600000 < Date.now()) {
     try {
       const res = await RutaApi.post("usuario/token/refresh", data);
       store.dispatch(setAuthToken(res.data));
