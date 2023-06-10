544f423d82ea66a8329fc9b38f2b8164a71c8979
Adlai CG <42752044+eriadlai@users.noreply.github.com>
2023-06-10 13:11:46 -0700

    Update
    Delete de clases que no se utilizan

--- cevit-frontend/src/app/store.jsx
+++ cevit-frontend/src/app/store.jsx
@@ -3,12 +3,10 @@ import { persistStore, persistReducer } from "redux-persist";
 import storage from "redux-persist/lib/storage";
 import userReducer from "../tools/userSlice";
 import tokenReducer from "../tools/authSlice";
-import themeReducer from "../tools/themeSlice";
 
 const rootReducer = combineReducers({
   usuario: userReducer,
   tokens: tokenReducer,
-  theme: themeReducer,
 });
 const persistConfig = {
   key: "root",
